3f5b25564e385c88349fc078a59282dc226aecf9
Steven Lang <steven.lang.mz@gmail.com>
2022-01-11 12:22:20 +0100

    Update
    A bit much to go into detail here. Fixed things here and there. Added a
    few new features, supported multi distribution layers, implement faster
    sampling via torch.gather and more I already forgot.
    
    Signed-off-by: Steven Lang <steven.lang.mz@gmail.com>

--- simple_einet/factorized_leaf_layer.py
+++ simple_einet/factorized_leaf_layer.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from .utils import SamplingContext
 from .layers import AbstractLayer
-from .distributions import Leaf
+from .distributions import AbstractLeaf
 
 
 class FactorizedLeaf(AbstractLayer):
@@ -19,7 +19,7 @@ class FactorizedLeaf(AbstractLayer):
 
     """
 
-    def __init__(self, in_features: int, out_features: int, num_repetitions, base_leaf: Leaf):
+    def __init__(self, num_features: int, num_features_out: int, num_repetitions, base_leaf: AbstractLeaf):
         """
         Args:
             in_features (int): Number of input features/RVs.
@@ -28,23 +28,23 @@ class FactorizedLeaf(AbstractLayer):
             base_leaf (Leaf): Base leaf distribution object.
         """
 
-        super().__init__(in_features, num_repetitions=num_repetitions)
+        super().__init__(num_features, num_repetitions=num_repetitions)
 
         self.base_leaf = base_leaf
-        self.out_features = out_features
+        self.num_features_out = num_features_out
 
         # Size of the factorized groups of RVs
-        cardinality = int(np.round(self.in_features / self.out_features))
+        cardinality = int(np.round(self.num_features / self.num_features_out))
 
         # Construct mapping of scopes from in_features -> out_features
-        scopes = torch.zeros(in_features, self.out_features, num_repetitions)
+        scopes = torch.zeros(num_features, self.num_features_out, num_repetitions)
         for r in range(num_repetitions):
-            idxs = torch.randperm(n=self.in_features)
-            for o in range(out_features):
+            idxs = torch.randperm(n=self.num_features)
+            for o in range(num_features_out):
                 low = o * cardinality
                 high = (o + 1) * cardinality
-                if o == out_features - 1:
-                    high = self.in_features
+                if o == num_features_out - 1:
+                    high = self.num_features
                 scopes[idxs[low:high], o, r] = 1
 
         self.register_buffer("scopes", scopes)
@@ -53,72 +53,100 @@ class FactorizedLeaf(AbstractLayer):
         # Forward through base leaf
         x = self.base_leaf(x, marginalized_scopes)
 
+        # Factorize input channels
+        x = x.sum(dim=1)
+
         # Merge scopes by naive factorization
         x = torch.einsum("bicr,ior->bocr", x, self.scopes)
+
+        assert x.shape == (x.shape[0], self.num_features_out, self.base_leaf.num_leaves, self.num_repetitions)
         return x
 
     def sample(self, num_samples: int = None, context: SamplingContext = None) -> torch.Tensor:
-        # Save original parent_indices and set context parent_indices to none, such that the out_channel
+        # Save original indices_out and set context indices_out to none, such that the out_channel
         # are not filtered in the base_leaf sampling procedure
-        parent_indices = context.parent_indices
-        context.parent_indices = None
+        indices_out = context.indices_out
+        context.indices_out = None
         samples = self.base_leaf.sample(context=context)
+        num_samples = samples.shape[0]
 
         # Check that shapes match as expected
-        if samples.dim() == 3:
+        if samples.dim() == 4:
             assert samples.shape == (
                 context.num_samples,
-                self.in_features,
-                self.base_leaf.out_channels,
+                self.base_leaf.num_channels,
+                self.num_features,
+                self.base_leaf.num_leaves,
             )
-        elif samples.dim() == 4:
-            assert self.in_features == samples.shape[1]
+        elif samples.dim() == 5:
+            assert self.num_features == samples.shape[1]
             assert hasattr(self.base_leaf, "cardinality")
             assert samples.shape == (
                 context.num_samples,
                 self.base_leaf.out_features,
                 self.base_leaf.cardinality,
-                self.base_leaf.out_channels,
+                self.base_leaf.num_leaves,
             )
 
-        # Collect final samples in temporary tensor
-        if hasattr(self.base_leaf, "cardinality"):
-            cardinality = self.base_leaf.cardinality
-        else:
-            cardinality = 1
-        tmp = torch.zeros(
-            context.num_samples,
-            self.in_features,
-            cardinality,
-            device=samples.device,
-            dtype=samples.dtype,
-        )
-        for sample_idx in range(context.num_samples):
-            # Get correct repetition
-            r = context.repetition_indices[sample_idx]
+        # indices_in_tmp = torch.zeros(num_samples, self.num_features, device=samples.device)
 
-            # Get correct parent_indices
-            parent_indices_out = parent_indices[sample_idx]
+        # # Collect final samples in temporary tensor
+        # if hasattr(self.base_leaf, "cardinality"):
+        #     cardinality = self.base_leaf.cardinality
+        # else:
+        #     cardinality = 1
+        # tmp = torch.zeros(
+        #     context.num_samples,
+        #     self.base_leaf.num_channels,
+        #     self.num_features,
+        #     cardinality,
+        #     device=samples.device,
+        #     dtype=samples.dtype,
+        # )
+        # for sample_idx in range(context.num_samples):
+        #     # Get correct repetition
+        #     r = context.indices_repetition[sample_idx]
 
-            # Get scope for the current repetition
-            scope = self.scopes[:, :, r]
+        #     # Get correct indices_out
+        #     indices_out_out = indices_out[sample_idx]
 
-            # Turn one-hot encoded in-feature -> out-feature mapping into a linear index
-            rnge_in = torch.arange(self.out_features, device=samples.device)
-            scope = (scope * rnge_in).sum(-1).long()
+        #     # Get scope for the current repetition
+        #     scope = self.scopes[:, :, r]
 
-            # Map parent_indices from original "out_features" view to "in_feautres" view
-            parent_indices_in = parent_indices_out[scope]
+        #     # Turn one-hot encoded in-feature -> out-feature mapping into a linear index
+        #     rnge_in = torch.arange(self.num_features_out, device=samples.device)
+        #     scope = (scope * rnge_in).sum(-1).long()
 
-            # Access base leaf samples based on
-            rnge_out = torch.arange(self.in_features, device=samples.device)
+        #     # Map indices_out from original "out_features" view to "in_feautres" view
+        #     indices_out_in = indices_out_out[scope]
+        #     indices_in_tmp[sample_idx] = indices_out_in
 
-            tmp[sample_idx] = samples[sample_idx, rnge_out, ..., parent_indices_in].view(
-                self.in_features, cardinality
-            )
+        #     # TODO: Alternative with gather which probably works over all samples
+        #     # scope_h = scope_h.view(-1, 1).expand(-1, indices_out_i.shape[0])
+        #     # scope_w = scope_w.view(1, -1).expand(self.in_shape[1], -1)
+        #     # indices_in = indices_out_i.gather(dim=0, index=scope_h)
 
-        samples = tmp.view(context.num_samples, -1)
+        #     # Access base leaf samples based on
+        #     rnge_out = torch.arange(self.num_features, device=samples.device)
+
+        #     sample_i = samples[sample_idx, :, rnge_out, ..., indices_out_in].view(
+        #         self.num_features, cardinality
+        #     )
+        #     tmp[sample_idx] = sample_i
+        # samples = tmp.view(context.num_samples, -1)
+
+        scopes = self.scopes[..., context.indices_repetition].permute(2, 0, 1)
+        rnge_in = torch.arange(self.num_features_out, device=samples.device)
+        scopes = (scopes * rnge_in).sum(-1).long()
+        indices_in_gather = indices_out.gather(dim=1, index=scopes)
+
+        # assert (indices_in_tmp == indices_in_gather).all()
+
+        indices_in_gather = indices_in_gather.view(num_samples, 1, -1, 1).expand(-1, samples.shape[1], -1, -1)
+        samples = samples.gather(dim=-1, index=indices_in_gather)
+        samples.squeeze_(-1)  # Remove num_leaves dimension
+
         return samples
 
-    def __repr__(self):
-        return f"FactorizedLeaf(in_features={self.in_features}, out_features={self.out_features})"
+    def extra_repr(self):
+        return f"num_features={self.num_features}, num_features_out={self.num_features_out}"
--- simple_einet/utils.py
+++ simple_einet/utils.py
@@ -1,12 +1,22 @@
 from contextlib import contextmanager, nullcontext
 from dataclasses import dataclass
+from typing import Tuple
 
+import numpy as np
 import torch
-from torch import nn
+from scipy.stats import rankdata
+from torch import Tensor, nn
+from tqdm.std import tqdm
+
 
 
 @contextmanager
-def provide_evidence(spn: nn.Module, evidence: torch.Tensor=None, marginalized_scopes:torch.Tensor=None, requires_grad=False):
+def provide_evidence(
+    spn: nn.Module,
+    evidence: torch.Tensor = None,
+    marginalized_scopes: torch.Tensor = None,
+    requires_grad=False,
+):
     """
     Context manager for sampling with evidence. In this context, the SPN graph is reweighted with the likelihoods
     computed using the given evidence.
@@ -48,10 +58,10 @@ class SamplingContext:
     num_samples: int = None
 
     # Indices into the out_channels dimension
-    parent_indices: torch.Tensor = None
+    indices_out: torch.Tensor = None
 
     # Indices into the repetition dimension
-    repetition_indices: torch.Tensor = None
+    indices_repetition: torch.Tensor = None
 
     # MPE flag, if true, will perform most probable explanation sampling
     is_mpe: bool = False
@@ -73,7 +83,7 @@ class SamplingContext:
 
     @property
     def is_root(self):
-        return self.parent_indices == None and self.repetition_indices == None
+        return self.indices_out == None and self.indices_repetition == None
 
 
 def invert_permutation(p: torch.Tensor):
@@ -85,3 +95,167 @@ def invert_permutation(p: torch.Tensor):
     s = torch.empty(p.shape[0], dtype=p.dtype, device=p.device)
     s[p] = torch.arange(p.shape[0], device=p.device)
     return s
+
+
+def calc_bpd(
+    log_p: Tensor, image_shape: Tuple[int, int, int], has_gauss_dist: bool, n_bins: int
+) -> float:
+    n_pixels = np.prod(image_shape)
+
+    if has_gauss_dist:
+        # https://github.com/openai/glow/blob/eaff2177693a5d84a1cf8ae19e8e0441715b82f8/model.py#L172
+        bpd = log_p - np.log(n_bins) * n_pixels
+        bpd = (-bpd / (np.log(2) * n_pixels)).mean()
+
+    else:
+        bpd = log_p - np.log(n_bins) * n_pixels
+        bpd = (-bpd / (np.log(2) * n_pixels)).mean()
+
+    return bpd
+
+
+def dequantize_image(image: Tensor, n_bins: int) -> Tensor:
+    return image + torch.rand_like(image) / n_bins
+
+
+def reduce_bits(image: Tensor, n_bits: int) -> Tensor:
+    image = image * 255
+    if n_bits < 8:
+        image = torch.floor(image / 2 ** (8 - n_bits))
+
+    return image
+
+
+def preprocess(
+    image: Tensor,
+    n_bits: int,
+    n_bins: int,
+    dequantize=True,
+    has_gauss_dist=True,
+) -> Tensor:
+    image = reduce_bits(image, n_bits)
+    if has_gauss_dist:
+        image = image / n_bins - 0.5
+        if dequantize:
+            image = dequantize_image(image, n_bins)
+    else:
+        image = image.long()
+
+    return image
+
+
+def rdc(x, y, f=np.sin, k=20, s=1 / 6.0, n=1):
+    """
+
+    Source: https://github.com/garydoranjr/rdc/blob/master/rdc/rdc.py
+
+    Computes the Randomized Dependence Coefficient
+    x,y: numpy arrays 1-D or 2-D
+         If 1-D, size (samples,)
+         If 2-D, size (samples, variables)
+    f:   function to use for random projection
+    k:   number of random projections to use
+    s:   scale parameter
+    n:   number of times to compute the RDC and
+         return the median (for stability)
+    According to the paper, the coefficient should be relatively insensitive to
+    the settings of the f, k, and s parameters.
+    """
+    if n > 1:
+        values = []
+        for i in range(n):
+            try:
+                values.append(rdc(x, y, f, k, s, 1))
+            except np.linalg.linalg.LinAlgError:
+                pass
+        return np.median(values)
+
+    if len(x.shape) == 1:
+        x = x.reshape((-1, 1))
+    if len(y.shape) == 1:
+        y = y.reshape((-1, 1))
+
+    # Copula Transformation
+    cx = np.column_stack([rankdata(xc, method="ordinal") for xc in x.T]) / float(x.size)
+    cy = np.column_stack([rankdata(yc, method="ordinal") for yc in y.T]) / float(y.size)
+
+    # Add a vector of ones so that w.x + b is just a dot product
+    O = np.ones(cx.shape[0])
+    X = np.column_stack([cx, O])
+    Y = np.column_stack([cy, O])
+
+    # Random linear projections
+    Rx = (s / X.shape[1]) * np.random.randn(X.shape[1], k)
+    Ry = (s / Y.shape[1]) * np.random.randn(Y.shape[1], k)
+    X = np.dot(X, Rx)
+    Y = np.dot(Y, Ry)
+
+    # Apply non-linear function to random projections
+    fX = f(X)
+    fY = f(Y)
+
+    # Compute full covariance matrix
+    C = np.cov(np.hstack([fX, fY]).T)
+
+    # Due to numerical issues, if k is too large,
+    # then rank(fX) < k or rank(fY) < k, so we need
+    # to find the largest k such that the eigenvalues
+    # (canonical correlations) are real-valued
+    k0 = k
+    lb = 1
+    ub = k
+    while True:
+
+        # Compute canonical correlations
+        Cxx = C[:k, :k]
+        Cyy = C[k0 : k0 + k, k0 : k0 + k]
+        Cxy = C[:k, k0 : k0 + k]
+        Cyx = C[k0 : k0 + k, :k]
+
+        eigs = np.linalg.eigvals(
+            np.dot(np.dot(np.linalg.pinv(Cxx), Cxy), np.dot(np.linalg.pinv(Cyy), Cyx))
+        )
+
+        # Binary search if k is too large
+        if not (np.all(np.isreal(eigs)) and 0 <= np.min(eigs) and np.max(eigs) <= 1):
+            ub -= 1
+            k = (ub + lb) // 2
+            continue
+        if lb == ub:
+            break
+        lb = k
+        if ub == lb + 1:
+            k = ub
+        else:
+            k = (ub + lb) // 2
+
+    return np.sqrt(np.max(eigs))
+
+
+if __name__ == '__main__':
+
+    from data import get_dataset
+    dataset = get_dataset(dataset_name="mnist", data_dir="/home/tak/data", train=True)
+    data = dataset.data.view(dataset.data.shape[0], -1)
+    D = data.shape[1]
+    for i in range(10):
+        # idx_x = torch.randperm(D)[:D // 2]
+        # idx_y = [i for i in range(D) if i not in idx_x]
+
+        # digit_x = 0
+        # digit_y = 1
+        # mask_x = dataset.targets == digit_x
+        # mask_y = dataset.targets == digit_y
+
+        # x = data[mask_x][:4000, idx_x].numpy()
+        # y = data[mask_y][:4000, idx_y].numpy()
+        x = data[:1000].numpy()
+
+        coeffs = np.zeros((D, D))
+
+        for i in tqdm(range(D)):
+            for j in range(D):
+                coeffs[i, j] = rdc(x[:, i], x[:, j], k=1)
+
+
+        print(coeffs)
