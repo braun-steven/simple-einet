a36b31de7d1c78f35b45c1fb051e81738cd77124
Steven Lang <steven.lang.mz@gmail.com>
2022-01-21 11:19:13 +0100

    Update setup.py
    Signed-off-by: Steven Lang <steven.lang.mz@gmail.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
@@ -10,7 +10,7 @@ setup(
     name="simple-einet",
     version="0.0.1",
     author="Steven Lang",
-    packages=["simple_einet"],
+    packages=find_packages(include=['simple_einet', 'simple_einet.*']),
     install_requires=required,
     long_description=long_description,
 )
