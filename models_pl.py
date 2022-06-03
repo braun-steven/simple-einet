11e116abff98ff93b8f2ffb855e1beda217267ee
Steven Lang <steven.lang.mz@gmail.com>
2022-06-03 09:02:42 +0200

    Cleanup lightning models
    Signed-off-by: Steven Lang <steven.lang.mz@gmail.com>

--- models_pl.py
+++ models_pl.py
@@ -2,7 +2,7 @@ from abc import ABC
 import argparse
 import os
 from argparse import Namespace
-from typing import Dict, Any
+from typing import Dict, Any, Tuple
 import numpy as np
 
 import torch
@@ -28,6 +28,10 @@ from simple_einet.einet import EinetConfig, Einet
 from simple_einet.distributions.binomial import Binomial
 
 
+# Translate the dataloader index to the dataset name
+DATALOADER_ID_TO_SET_NAME = {0: "val", 1: "test"}
+
+
 def make_einet(args, num_classes: int = 1):
     """
     Make an EinsumNetworks model based off the given arguments.
@@ -40,7 +44,9 @@ def make_einet(args, num_classes: int = 1):
     """
     image_shape = get_data_shape(args.dataset)
     # leaf_kwargs, leaf_type = {"total_count": 255}, Binomial
-    leaf_kwargs, leaf_type = get_distribution(dist=args.dist, min_sigma=args.min_sigma, max_sigma=args.max_sigma)
+    leaf_kwargs, leaf_type = get_distribution(
+        dist=args.dist, min_sigma=args.min_sigma, max_sigma=args.max_sigma
+    )
 
     config = EinetConfig(
         num_features=image_shape.num_pixels,
@@ -89,26 +95,23 @@ class LitModel(pl.LightningModule, ABC):
         self.rtpt.start()
 
 
-
 class SpnGenerative(LitModel):
     def __init__(self, args: Namespace):
         super().__init__(args=args, name="gen")
         self.spn = make_einet(args)
 
-
-
     def training_step(self, train_batch, batch_idx):
         data, labels = train_batch
         data = self.preprocess(data)
         nll = self.negative_log_likelihood(data)
-        self.log("train_nll", nll)
+        self.log("Train/nll", nll)
         return nll
 
     def validation_step(self, val_batch, batch_idx):
         data, labels = val_batch
         data = self.preprocess(data)
         nll = self.negative_log_likelihood(data)
-        self.log("val_nll", nll)
+        self.log("Val/nll", nll)
         return nll
 
     def negative_log_likelihood(self, data):
@@ -133,7 +136,6 @@ class SpnGenerative(LitModel):
 
     def on_train_epoch_end(self):
 
-
         with torch.no_grad():
             samples = self.generate_samples(num_samples=25)
             grid = torchvision.utils.make_grid(
@@ -142,7 +144,6 @@ class SpnGenerative(LitModel):
             self.logger.log_image(key="samples", images=[grid])
 
         super().on_train_epoch_end()
-
 
     def save_samples(self, samples_dir, num_samples, nrow):
         """
@@ -161,52 +162,59 @@ class SpnGenerative(LitModel):
             )
             torchvision.utils.save_image(grid, os.path.join(samples_dir, f"{i}.png"))
 
-    def test_step(self, batch, batch_idx):
+    def test_step(self, batch, batch_idx, dataloader_id=0):
         data, labels = batch
 
         data = self.preprocess(data)
         nll = self.negative_log_likelihood(data)
 
-        # Log
-        self.log("test_nll", nll)
+        set_name = DATALOADER_ID_TO_SET_NAME[dataloader_id]
+        self.log(f"Test/{set_name}_nll", nll, add_dataloader_idx=False)
 
 
 class SpnDiscriminative(LitModel):
+    """
+    Discriminative SPN model. Models the class conditional data distribution at its C root nodes.
+    """
     def __init__(self, args: Namespace):
         super().__init__(args, name="disc")
+
+        # Construct SPN
         self.spn = make_einet(args, num_classes=10)
-        self.image_shape = get_data_shape(args.dataset)
 
-
+        # Define loss function
         self.criterion = nn.CrossEntropyLoss()
 
     def training_step(self, train_batch, batch_idx):
-        data, labels = train_batch
-        data = self.preprocess(data)
-        outputs = self.spn(data)  # [N, C]
-        loss = self.criterion(outputs, labels)
-        accuracy = (labels == outputs.argmax(-1)).sum() / outputs.shape[0]
-        self.log("train_accuracy", accuracy, prog_bar=True)
-        self.log("train_cross_entropy", loss)
+        loss, accuracy = self._get_cross_entropy_and_accuracy(train_batch)
+        self.log("Train/accuracy", accuracy, prog_bar=True)
+        self.log("Train/cross_entropy", loss)
         return loss
 
     def validation_step(self, val_batch, batch_idx):
-        data, labels = val_batch
+        loss, accuracy = self._get_cross_entropy_and_accuracy(val_batch)
+        self.log("Val/cross_entropy", loss)
+        self.log("Val/accuracy", accuracy)
+        return loss
+
+    def test_step(self, batch, batch_idx, dataloader_id=0):
+        loss, accuracy = self._get_cross_entropy_and_accuracy(batch)
+        set_name = DATALOADER_ID_TO_SET_NAME[dataloader_id]
+        self.log(f"Test/{set_name}_cross_entropy", loss, add_dataloader_idx=False)
+        self.log(f"Test/{set_name}_accuracy", accuracy, add_dataloader_idx=False)
+
+    def _get_cross_entropy_and_accuracy(self, batch) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Compute cross entropy loss and accuracy of batch.
+        Args:
+            batch: Batch of data.
+
+        Returns:
+            Tuple of (cross entropy loss, accuracy).
+        """
+        data, labels = batch
         data = self.preprocess(data)
         outputs = self.spn(data)  # [N, C]
         loss = self.criterion(outputs, labels)
-        self.log("val_cross_entropy", loss)
         accuracy = (labels == outputs.argmax(-1)).sum() / outputs.shape[0]
-        self.log("val_accuracy", accuracy)
-        return loss
-
-    def test_step(self, batch, batch_idx):
-        data, labels = batch
-        data = self.preprocess(data)
-
-        outputs = self.spn(data)
-
-        loss = self.criterion(outputs, labels)
-        accuracy = (labels == outputs.argmax(-1)).sum() / outputs.shape[0]
-        self.log("test_cross_entropy", loss)
-        self.log("test_accuracy", accuracy)
+        return loss, accuracy
