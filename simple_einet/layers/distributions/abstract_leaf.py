Fix wrong softmax projection on probs/logits (John Leland)

simple_einet/layers/distributions/abstract_leaf.py
@@ -112,7 +112,7 @@ def dist_sample(distribution: dist.Distribution, ctx: SamplingContext = None) ->
         elif type(distribution) == CustomNormal:
             distribution = CustomNormal(mu=distribution.mu, sigma=distribution.sigma / ctx.temperature_leaves)
         elif type(distribution) == dist.Categorical:
-            distribution = dist.Categorical(logits=F.log_softmax(distribution.probs / ctx.temperature_leaves))
+            distribution = dist.Categorical(logits=F.log_softmax(distribution.logits / ctx.temperature_leaves))
         samples = distribution.sample(sample_shape=(ctx.num_samples,)).float()
 
     assert (