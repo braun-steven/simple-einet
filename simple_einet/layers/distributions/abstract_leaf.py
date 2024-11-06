fix(cat): correct temperature scaling

simple_einet/layers/distributions/abstract_leaf.py
@@ -124,7 +124,7 @@ def dist_sample(distribution: dist.Distribution, ctx: SamplingContext = None) ->
             elif type(distribution) == CustomNormal:
                 distribution = CustomNormal(mu=distribution.mu, sigma=distribution.sigma * np.sqrt(ctx.temperature_leaves))
             elif type(distribution) == dist.Categorical:
-                distribution = dist.Categorical(logits=F.softmax(distribution.logits / ctx.temperature_leaves))
+                distribution = dist.Categorical(logits=distribution.logits / ctx.temperature_leaves)
 
             samples = distribution.sample(sample_shape=(ctx.num_samples,)).float()
 